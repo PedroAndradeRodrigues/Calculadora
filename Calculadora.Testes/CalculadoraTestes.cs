1f7d59a0531fc4a9f5fd8b2fd83b66de9be4c548
PedroAndradeRodrigues <pedro.andrade.rodrigues@gmail.com>
2026-04-13 14:24:07 -0300

    Classe de Testes - XUnit

---
+++ Calculadora.Testes/CalculadoraTestes.cs
@@ -0,0 +1,26 @@
+﻿namespace Calculadora.Testes
+{
+    public class CalculadoraTestes
+    {
+        [Fact]
+        public void Somar()
+        {
+            Assert.Equal(20, Calculadora.Somar(10, 10));
+        }
+
+        public void Subtrair()
+        {
+            Assert.Equal(0, Calculadora.Subtrair(10, 10));
+        }
+
+        public void Multiplicar()
+        {
+            Assert.Equal(100, Calculadora.Multiplicar(10, 10));
+        }
+
+        public void Dividir()
+        {
+            Assert.Equal(1, Calculadora.Dividir(10, 10));
+        }
+    }
+}
